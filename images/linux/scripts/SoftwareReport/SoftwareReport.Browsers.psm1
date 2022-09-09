6e9ba565b46e9b5c4df3cde45864455a8b78bc9b
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:13:26 -0500

    Initialize releases/ubuntu18/20210131

---
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Browsers.psm1
@@ -0,0 +1,24 @@
+function Get-ChromeVersion {
+    $googleChromeVersion = google-chrome --version | Take-OutputPart -Part 2
+    return "Google Chrome $googleChromeVersion"
+}
+
+function Get-ChromeDriverVersion {
+    $chromeDriverVersion = chromedriver --version | Take-OutputPart -Part 1
+    return "ChromeDriver $chromeDriverVersion"
+}
+
+function Get-FirefoxVersion {
+    $firefoxVersion = firefox --version
+    return $firefoxVersion
+}
+
+function Get-GeckodriverVersion {
+    $geckodriverVersion = geckodriver --version | Select-Object -First 1 | Take-OutputPart -Part 1
+    return "Geckodriver $geckodriverVersion"
+}
+
+function Get-ChromiumVersion {
+    $chromiumVersion = chromium --version | Take-OutputPart -Part 0,1
+    return $chromiumVersion
+}
