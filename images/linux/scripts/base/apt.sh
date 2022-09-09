b969dc6500d0fb8643daf064736ca8645bc64360
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:12:18 -0500

    Initialize releases/win19/20201108

---
+++ images/linux/scripts/base/apt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# Stop and disable apt-daily upgrade services;
+systemctl stop apt-daily.timer
+systemctl disable apt-daily.timer
+systemctl disable apt-daily.service
+systemctl stop apt-daily-upgrade.timer
+systemctl disable apt-daily-upgrade.timer
+systemctl disable apt-daily-upgrade.service
+
+# This step should completely disable any automatic updates except manual
+sudo sed -i 's/APT::Periodic::Update-Package-Lists "1"/APT::Periodic::Update-Package-Lists "0"/' /etc/apt/apt.conf.d/20auto-upgrades
+
+# Enable retry logic for apt up to 10 times
+echo "APT::Acquire::Retries \"10\";" > /etc/apt/apt.conf.d/80-retries
+
+# Configure apt to always assume Y
+echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
+
+# Install aria2 and jq
+apt-get install aria2
+apt-get install jq
+# Use apt-fast for parallel downloads
+add-apt-repository -y ppa:apt-fast/stable
+apt-get update
+apt-get install apt-fast
