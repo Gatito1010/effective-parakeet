9c547476499c1ae084fbfa6e9c9f029a5ab0e289
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:11:05 -0500

    Initialize releases/win16/20200917

---
+++ images/linux/scripts/installers/subversion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+################################################################################
+##  File:  subversion.sh
+##  Desc:  Installs Subversion client
+################################################################################
+
+
+# Install Subversion
+apt-get install -y --no-install-recommends subversion
+
+# Run tests to determine that the software installed as expected
+echo "Testing to make sure that script performed as expected, and basic scenarios work"
+if ! command -v svn; then
+    echo "Subversion (svn) was not installed"
+    exit 1
+fi
