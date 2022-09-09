5bbba5a952d2e87aa605fc8a0a102d116a0496b7
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:18:50 -0500

    Initialize releases/macOS-10.14/20210918

---
+++ images/win/scripts/Installers/Install-Docker.ps1
@@ -0,0 +1,26 @@
+################################################################################
+##  File:  Install-Docker.ps1
+##  Desc:  Install Docker.
+##         Must be an independent step becuase it requires a restart before we
+##         can continue.
+################################################################################
+
+Write-Host "Install-Package Docker"
+Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+Start-Service docker
+
+Write-Host "Install-Package Docker-Compose"
+Choco-Install -PackageName docker-compose
+
+$dockerImages = (Get-ToolsetContent).docker.images
+foreach ($dockerImage in $dockerImages) {
+    Write-Host "Pulling docker image $dockerImage ..."
+    docker pull $dockerImage
+
+    if (!$?) {
+        Write-Host "Docker pull failed with a non-zero exit code"
+        exit 1
+    }
+}
+
+Invoke-PesterTests -TestFile "Docker"
