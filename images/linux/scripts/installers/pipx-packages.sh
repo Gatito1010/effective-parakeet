5b62b826112d4d7a178a5eb1f2f2be486e43d076
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:17:12 -0500

    Initialize releases/macOS-10.15/20210712

---
+++ images/linux/scripts/installers/pipx-packages.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+################################################################################
+##  File:  pipx-packages.sh
+##  Desc:  Install tools via pipx
+################################################################################
+source $HELPER_SCRIPTS/install.sh
+
+export PATH="$PATH:/opt/pipx_bin"
+
+pipx_packages=$(get_toolset_value ".pipx[] .package")
+
+for package in $pipx_packages; do
+    python_version=$(get_toolset_value ".pipx[] | select(.package == \"$package\") .python")
+    if [ "$python_version" != "null" ]; then
+        python_path="/opt/hostedtoolcache/Python/$python_version*/x64/bin/python$python_version"
+        echo "Install $package into python $python_path"
+        pipx install $package --python $python_path
+    else
+        echo "Install $package into default python"
+        pipx install $package
+
+        # Install ansible into an existing ansible-base Virtual Environment
+        if [[ $package == "ansible-base" ]]; then
+            pipx inject ansible-base ansible
+        fi
+    fi
+
+done
+
+invoke_tests "Common" "PipxPackages"
