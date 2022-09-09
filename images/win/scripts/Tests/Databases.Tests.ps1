a3db2d87d401f19659fe296c1f86667630c393c2
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:20:11 -0500

    Initialize releases/win16/20211115

---
+++ images/win/scripts/Tests/Databases.Tests.ps1
@@ -0,0 +1,61 @@
+Describe "MongoDB" {
+    It "<ToolName>" -TestCases @(
+        @{ ToolName = "mongo" }
+        @{ ToolName = "mongod" }
+    ) {
+        $toolsetVersion = (Get-ToolsetContent).mongodb.version
+        (&$ToolName --version)[2].Split('"')[-2] | Should -BeLike "$toolsetVersion*"
+    }
+}
+
+Describe "PostgreSQL" {
+    $psqlTests = @(
+        @{envVar = "PGROOT"; pgPath = Get-EnvironmentVariable "PGROOT"}
+        @{envVar = "PGBIN"; pgPath = Get-EnvironmentVariable "PGBIN"}
+        @{envVar = "PGDATA"; pgPath = Get-EnvironmentVariable "PGDATA"}
+    )
+
+    Context "Environment variable" {
+        It "PGUSER contains postgres" {
+            Get-EnvironmentVariable "PGUSER" | Should -Be "postgres"
+        }
+
+        It "PGPASSWORD contains root" {
+            Get-EnvironmentVariable "PGPASSWORD" | Should -Be "root"
+        }
+
+        It "<envVar> environment variable exists" -TestCases $psqlTests {
+            Get-EnvironmentVariable $envVar | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    Context "Path" {
+        It "<pgPath> path exists" -TestCases $psqlTests {
+            $pgPath | Should -Exist
+        }
+    }
+
+    Context "Service" {
+        $psqlService = Get-Service -Name postgresql*
+        $psqlServiceTests = @{
+            Name = $psqlService.Name
+            Status = $psqlService.Status
+            StartType = $psqlService.StartType
+        }
+
+        It "<Name> service is stopped" -TestCases $psqlServiceTests {
+            $Status | Should -Be "Stopped"
+        }
+
+        It "<Name> service is disabled" -TestCases $psqlServiceTests {
+            $StartType | Should -Be "Disabled"
+        }
+    }
+}
+
+Describe "MySQL" {
+    It "MySQL CLI" {
+        $MysqlMajorMinor = (Get-ToolsetContent).Mysql.version
+        mysql -V | Should -BeLike "*${MysqlMajorMinor}*"
+    }
+}
