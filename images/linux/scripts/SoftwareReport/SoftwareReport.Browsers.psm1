0d9c4dd2541c091c562535b9c556582173288dc8
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:19:27 -0500

    Initialize releases/macOS-10.14/20211016

---
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Browsers.psm1
@@ -0,0 +1,42 @@
+function Get-ChromeVersion {
+    $googleChromeVersion = google-chrome --version | Take-OutputPart -Part 2
+    return "Google Chrome $googleChromeVersion"
+}
+
+function Get-ChromeDriverVersion {
+    $chromeDriverVersion = chromedriver --version | Take-OutputPart -Part 1
+    return "ChromeDriver $chromeDriverVersion"
+}
+
+function Get-FirefoxVersion {
+    $firefoxVersion = firefox --version
+    return $firefoxVersion
+}
+
+function Get-GeckodriverVersion {
+    $geckodriverVersion = geckodriver --version | Select-Object -First 1 | Take-OutputPart -Part 1
+    return "Geckodriver $geckodriverVersion"
+}
+
+function Get-ChromiumVersion {
+    $chromiumVersion = chromium-browser --version | Take-OutputPart -Part 0,1
+    return $chromiumVersion
+}
+
+function Build-BrowserWebdriversEnvironmentTable {
+    return @(
+        @{
+            "Name" = "CHROMEWEBDRIVER"
+            "Value" = $env:CHROMEWEBDRIVER
+        },
+        @{
+            "Name" = "GECKOWEBDRIVER"
+            "Value" = $env:GECKOWEBDRIVER
+        }
+    ) | ForEach-Object {
+        [PSCustomObject] @{
+            "Name" = $_.Name
+            "Value" = $_.Value
+        }
+    }
+}
