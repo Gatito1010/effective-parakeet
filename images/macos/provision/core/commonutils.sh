7917552ffa46f5af71bb88322986b61c12133514
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:12:58 -0500

    Initialize releases/ubuntu18/20210117

---
+++ images/macos/provision/core/commonutils.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e -o pipefail
+source ~/utils/utils.sh
+source ~/utils/invoke-tests.sh
+
+common_packages=$(get_toolset_value '.brew.common_packages[]')
+for package in $common_packages; do
+    echo "Installing $package..."
+    brew_smart_install "$package"
+done
+
+cask_packages=$(get_toolset_value '.brew.cask_packages[]')
+for package in $cask_packages; do
+    echo "Installing $package..."
+    brew install --cask $package
+done
+
+# Invoke bazel to download the latest bazel version via bazelisk
+bazel
+
+# Invoke tests for all common tools
+invoke_tests "Common" "CommonUtils"
