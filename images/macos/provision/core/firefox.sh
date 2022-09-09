9618a6ee7dc727c71d68d10f88e6cd127705ad35
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:13:14 -0500

    Initialize releases/macOS-10.15/20210123

---
+++ images/macos/provision/core/firefox.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e -o pipefail
+source ~/utils/utils.sh
+
+echo "Installing Firefox..."
+brew install --cask firefox
+
+echo "Installing Geckodriver..."
+brew_smart_install "geckodriver"
+
+echo "Add GECKOWEBDRIVER to bashrc..."
+echo "export GECKOWEBDRIVER=$(brew --prefix geckodriver)/bin" >> "${HOME}/.bashrc"
+
+invoke_tests "Browsers" "Firefox"
