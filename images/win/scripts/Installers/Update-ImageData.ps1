a5449f3dad30dc35a69cd5c93492b8bf5a9660c7
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:17:40 -0500

    Initialize releases/ubuntu20/20210803

---
+++ images/win/scripts/Installers/Update-ImageData.ps1
@@ -0,0 +1,43 @@
+$os = Get-CimInstance -ClassName Win32_OperatingSystem
+$caption = $os.Caption
+$osName = $caption.Substring(0, $caption.LastIndexOf(" "))
+$osEdition = $caption.Substring($caption.LastIndexOf(" ")+1)
+$osVersion = $os.Version
+$imageVersion = $env:IMAGE_VERSION
+$imageDataFile = $env:IMAGEDATA_FILE
+$githubUrl="https://github.com/actions/virtual-environments/blob"
+
+if ($caption -match "2019")
+{
+    $imageLabel = "windows-2019"
+    $softwareUrl = "${githubUrl}/win19/${imageVersion}/images/win/Windows2019-Readme.md"
+    $releaseUrl="https://github.com/actions/virtual-environments/releases/tag/win19%2F${imageVersion}"
+}
+
+if ($caption -match "2016")
+{
+    $imageLabel = "windows-2016"
+    $softwareUrl = "${githubUrl}/win16/${imageVersion}/images/win/Windows2016-Readme.md"
+    $releaseUrl="https://github.com/actions/virtual-environments/releases/tag/win16%2F${imageVersion}"
+}
+
+$json = @"
+[
+  {
+    "group": "Operating System",
+    "detail": "${osName}\n${osVersion}\n${osEdition}"
+  },
+  {
+    "group": "Virtual Environment",
+    "detail": "Environment: ${imageLabel}\nVersion: ${imageVersion}\nIncluded Software: ${softwareUrl}\nImage Release: ${releaseUrl}"
+  }
+]
+"@
+
+$json | Out-File -FilePath $imageDataFile
+
+
+# Set static env vars
+setx ImageVersion $env:IMAGE_VERSION /m
+setx ImageOS $env:IMAGE_OS /m
+setx AGENT_TOOLSDIRECTORY $env:AGENT_TOOLSDIRECTORY /m
