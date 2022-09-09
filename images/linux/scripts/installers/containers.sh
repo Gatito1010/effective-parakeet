ce81a394328d4417873fe42e0f61d588a5ec0297
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:14:54 -0500

    Initialize releases/win16/20210404

---
+++ images/linux/scripts/installers/containers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+################################################################################
+##  File:  containers.sh
+##  Desc:  Installs container tools: podman, buildah and skopeo onto the image
+################################################################################
+
+# Install podman, buildah, scopeo container's tools
+install_packages=(podman buildah skopeo)
+source /etc/os-release
+sh -c "echo 'deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+wget -qnv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
+apt-key add Release.key
+apt-get update -qq
+apt-get -qq -y install ${install_packages[@]}
+mkdir -p /etc/containers
+echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | tee /etc/containers/registries.conf
+
+invoke_tests "Tools" "Containers"
