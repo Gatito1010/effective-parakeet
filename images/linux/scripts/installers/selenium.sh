073575e8054a7af116eca3dec91dd0ad518146a2
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:16:14 -0500

    Initialize releases/win16/20210525

---
+++ images/linux/scripts/installers/selenium.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+################################################################################
+##  File:  selenium.sh
+##  Desc:  Installs selenium server
+################################################################################
+
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/install.sh
+
+# Determine latest selenium standalone server version
+SELENIUM_LATEST_VERSION_URL=https://api.github.com/repos/SeleniumHQ/selenium/releases/latest
+SELENIUM_VERSION=$(curl $SELENIUM_LATEST_VERSION_URL | jq '.name' | tr -d '"' | cut -d ' ' -f 2)
+SELENIUM_VERSION_MAJOR_MINOR=$(echo $SELENIUM_VERSION | cut -d '.' -f 1,2)
+
+# Download selenium standalone server
+echo "Downloading selenium-server-standalone v$SELENIUM_VERSION..."
+SELENIUM_JAR_NAME="selenium-server-standalone-$SELENIUM_VERSION.jar"
+download_with_retries https://selenium-release.storage.googleapis.com/$SELENIUM_VERSION_MAJOR_MINOR/$SELENIUM_JAR_NAME
+
+SELENIUM_JAR_PATH="/usr/share/java/selenium-server-standalone.jar"
+mv $SELENIUM_JAR_NAME $SELENIUM_JAR_PATH
+echo "SELENIUM_JAR_PATH=$SELENIUM_JAR_PATH" | tee -a /etc/environment
+
+invoke_tests "Tools" "Selenium"
