e19f1bb1b38c9f2aa1efe078f063bc9e7e18f314
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:19:05 -0500

    Initialize releases/ubuntu20/20210929

---
+++ images/macos/provision/core/openssl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e -o pipefail
+source ~/utils/utils.sh
+
+echo "Install openssl@1.1"
+brew_smart_install "openssl@1.1"
+
+# Symlink brew openssl@1.1 to `/usr/local/bin` as Homebrew refuses
+ln -sf $(brew --prefix openssl@1.1)/bin/openssl /usr/local/bin/openssl
+
+invoke_tests "OpenSSL"
