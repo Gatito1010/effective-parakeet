0fc0f0f6a6a7299993c175b5eb82d63115bbac52
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:14:09 -0500

    Initialize releases/macOS-11.0/20210302

---
+++ images/macos/tests/Common.Tests.ps1
@@ -0,0 +1,88 @@
+Import-Module "$PSScriptRoot/../helpers/Common.Helpers.psm1"
+Import-Module "$PSScriptRoot/../helpers/Tests.Helpers.psm1" -DisableNameChecking
+
+$os = Get-OSVersion
+
+Describe ".NET" {
+    It ".NET" {
+        "dotnet --version" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe "GCC" -Skip:($os.IsHighSierra) {
+    $testCases = @("8", "9", "10") | ForEach-Object { @{Version = $_} }
+
+    It "GCC <Version>" -TestCases $testCases {
+        param (
+            [string] $Version
+        )
+
+        "gcc-$Version --version" | Should -ReturnZeroExitCode
+    }
+
+    It "Gfortran <Version>" -TestCases $testCases {
+        param (
+            [string] $Version
+        )
+
+        "gfortran-$Version --version" | Should -ReturnZeroExitCode
+    }
+
+    It "Gfortran is not found in the default path" {
+        "$(which gfortran)" | Should -BeNullOrEmpty
+    }
+}
+
+Describe "vcpkg" -Skip:($os.IsHighSierra -or $os.IsMojave) {
+    It "vcpkg" {
+        "vcpkg version" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe "AWS" {
+    It "AWS CLI" {
+        "aws --version" | Should -ReturnZeroExitCode
+    }
+    It "AWS SAM CLI" {
+        "sam --version" | Should -ReturnZeroExitCode
+    }
+
+    It "AWS Session Manager CLI" {
+        "session-manager-plugin --version" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe "AzCopy" {
+    It "AzCopy" {
+        "azcopy --version" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe "Miniconda" {
+    It "Conda" {
+        Get-EnvironmentVariable "CONDA" | Should -Not -BeNullOrEmpty
+        $condaBinPath = Join-Path $env:CONDA "bin" "conda"
+        "$condaBinPath --version" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe "Stack" -Skip:($os.IsHighSierra) {
+    It "Stack" {
+        "stack --version" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe "CocoaPods" {
+    It "CocoaPods" {
+        "pod --version" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe "VSMac" {
+    It "VS4Mac is installed" {
+        $vsPath = "/Applications/Visual Studio.app"
+        $vstoolPath = Join-Path $vsPath "Contents/MacOS/vstool"
+        $vsPath | Should -Exist
+        $vstoolPath | Should -Exist
+    }
+}
