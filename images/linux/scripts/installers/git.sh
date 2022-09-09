ef5505bad5adad5f8fedd35ac368fbe6dcd39cf4
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:11:11 -0500

    Initialize releases/win16/20200920

---
+++ images/linux/scripts/installers/git.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+################################################################################
+##  File:  git.sh
+##  Desc:  Installs Git
+################################################################################
+set -e
+
+# Source the helpers for use with the script
+source "$HELPER_SCRIPTS"/install.sh
+
+## Install git
+add-apt-repository ppa:git-core/ppa -y
+apt-get update
+apt-get install git -y
+git --version
+
+# Install git-lfs
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install -y --no-install-recommends git-lfs
+
+# Install git-ftp
+apt-get install git-ftp -y
+
+# Run tests to determine that the software installed as expected
+echo "Testing git installation"
+if ! command -v git; then
+    echo "git was not installed"
+    exit 1
+fi
+echo "Testing git-lfs installation"
+if ! command -v git-lfs; then
+    echo "git-lfs was not installed"
+    exit 1
+fi
+echo "Testing git-ftp installation"
+if ! command -v git-ftp; then
+    echo "git-ftp was not installed"
+    exit 1
+fi
+
+#Install hub
+tmp_hub="/tmp/hub"
+mkdir -p "$tmp_hub"
+url=$(curl -s https://api.github.com/repos/github/hub/releases/latest | jq -r '.assets[].browser_download_url | select(contains("hub-linux-amd64"))')
+download_with_retries "$url" "$tmp_hub"
+tar xzvf "$tmp_hub"/hub-linux-amd64-*.tgz --strip-components 1 -C "$tmp_hub"
+mv "$tmp_hub"/bin/hub /usr/local/bin
+
+if command -v hub; then
+    echo "hub CLI was installed successfully"
+else
+    echo "[!] Hub CLI was not installed"
+    exit 1
+fi
---
+++ images/linux/scripts/installers/image-magick.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+################################################################################
+##  File:  image-magick.sh
+##  Desc:  Installs ImageMagick
+################################################################################
+
+
+# Install ImageMagick
+apt-get install -y --no-install-recommends --fix-missing \
+    imagemagick \
+    libmagickcore-dev \
+    libmagickwand-dev \
+    libmagic-dev
