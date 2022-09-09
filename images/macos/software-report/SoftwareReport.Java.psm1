7bc2ae1b960d30d07a0e8e95a1294554ae87c406
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:15:42 -0500

    Initialize releases/ubuntu18/20210504

---
+++ images/macos/software-report/SoftwareReport.Java.psm1
@@ -0,0 +1,22 @@
+function Get-JavaVersions {
+    $defaultJavaPath = Get-Item env:JAVA_HOME
+    $javaVersions = Get-Item env:JAVA_HOME_*_X64
+    $sortRules = @{
+        Expression = { [Int32]$_.Name.Split("_")[2] }  
+        Descending = $false
+    }
+
+    return $javaVersions | Sort-Object $sortRules | ForEach-Object {
+        $javaPath = $_.Value
+        # Take semver from the java path
+        $version = $javaPath.split('/')[5]
+        $fullVersion = $version.Replace('-', '+')
+        $defaultPostfix = ($javaPath -eq $defaultJavaPath) ? " (default)" : ""
+
+        [PSCustomObject] @{
+            "Version" = $fullVersion + $defaultPostfix
+            "Vendor" = "Adopt OpenJDK"
+            "Environment Variable" = $_.Name
+        }
+    }
+}
