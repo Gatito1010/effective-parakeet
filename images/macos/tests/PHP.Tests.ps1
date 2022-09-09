4a21f9957114fc64cf4434c4b6ffad943afb90f6
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:18:56 -0500

    Initialize releases/macOS-11/20210927

---
+++ images/macos/tests/PHP.Tests.ps1
@@ -0,0 +1,17 @@
+$os = Get-OSVersion
+
+Describe "PHP" -Skip:($os.IsMonterey) {
+    Context "PHP" {
+        It "PHP Path" {
+            Get-WhichTool "php" | Should -Not -BeLike "/usr/bin/php*"
+        }
+        It "PHP version" {
+            "php --version" | Should -ReturnZeroExitCode
+        }
+    }
+    Context "Composer" {
+        It "Composer" {
+            "composer --version" | Should -ReturnZeroExitCode
+        }
+    }
+}
