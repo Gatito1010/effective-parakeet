4a6c5169cd55e9fbbc1697e71a62f9d54a332422
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:11:42 -0500

    Initialize releases/win19/20201021

---
+++ images/macos/provision/core/aws.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Installing aws...
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
+rm -rf AWSCLIV2.pkg
+
+echo Installing aws sam cli...
+brew tap aws/tap
+brew install aws-sam-cli
+
+echo "Install aws cli session manager"
+brew cask install session-manager-plugin
