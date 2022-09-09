cb892aa05908426608c91353fc635651851aecc4
Israel hernandez <110138321+Gatito1010@users.noreply.github.com>
2022-09-09 02:11:32 -0500

    Initialize releases/ubuntu16/20201012

---
+++ images/linux/scripts/installers/heroku.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+################################################################################
+##  File:  heroku.sh
+##  Desc:  Installs Heroku CLI
+################################################################################
+
+
+# Install Heroku CLI
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+# Run tests to determine that the software installed as expected
+echo "Testing to make sure that script performed as expected, and basic scenarios work"
+if ! command -v heroku; then
+    exit 1
+fi
